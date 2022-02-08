e781adf6ceebf558eead7e9fea3729b87ba6c214
Thais <tfrancag@gmail.com>
2022-02-08 14:45:09 -0300

    Alteraçòes de preparo para deploy

--- src/app.js
+++ src/app.js
@@ -16,6 +16,6 @@ app.get("/checkBalance", checkBalance);
 
 app.post("/", addIncome);
 
-app.listen(5000, () => {
-  console.log("Server is running at http://localhost:5000/");
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
